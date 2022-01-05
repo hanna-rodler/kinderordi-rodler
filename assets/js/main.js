8b1045179ff2981fb80bae1ae418636116283406
Hanna Rodler <63262915+hanna-rodler@users.noreply.github.com>
2022-01-05 19:15:26 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,23 +2,29 @@ $(function () {
     let btnTop = document.querySelector(".to-top");
     btnTop.addEventListener("click", scrollToTop);
 
-    $(function() {
-        var windowHeight = $(window).height();
-        var contHeight = $("main").height() + $("header").height();
-        var footerHeight = $("footer").height();
-        var testHeight = windowHeight - footerHeight;
+    stickFooterToBttom();
 
-        if (contHeight < testHeight) {
-            $("footer").addClass("fixed-bottom");
-         /*   $("footer").css("bottom", "0");
-            $("footer").css("left", "0");*/
-            console.log("hi");
-        }
-        console.log("hi");
+    $(window).on('resize', function () {
+        stickFooterToBttom();
     });
 });
 
-function scrollToTop(){
-    document.body.scrollTop =0;
-    document.documentElement.scrollTop=0;
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function stickFooterToBttom() {
+    let footer = $("footer");
+    let windowHeight = $(window).height();
+    let contHeight = $("main").height() + $("header").height();
+    let footerHeight = footer.height();
+    let testHeight = windowHeight - footerHeight;
+
+    if (contHeight < testHeight) {
+        footer.addClass("fixed-bottom");
+    }
+    if (contHeight > testHeight) {
+        footer.removeClass("fixed-bottom");
+    }
 }
